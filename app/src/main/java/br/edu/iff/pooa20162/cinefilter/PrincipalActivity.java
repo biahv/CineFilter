4aa7f0dcb82708f999822cbc5ca0cc3b19ce1397
biahv <beatriz.hviana@gmail.com>
2016-12-12 22:48:47 -0200

    Atualizacao 2.0

--- app/src/main/java/br/edu/iff/pooa20162/cinefilter/PrincipalActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/cinefilter/PrincipalActivity.java
@@ -1,7 +1,10 @@
 package br.edu.iff.pooa20162.cinefilter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +12,59 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        ImageButton imageCinema = (ImageButton) findViewById(R.id.imageCinema);
+        imageCinema.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this,CinemaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageFilme = (ImageButton) findViewById(R.id.imageFilme);
+        imageFilme.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this,FilmeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageSessao = (ImageButton) findViewById(R.id.imageSessao);
+        imageSessao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this,SessaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imagePesquisa = (ImageButton) findViewById(R.id.imagePesquisa);
+        imagePesquisa.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this,PesquisaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageConfiguracao = (ImageButton) findViewById(R.id.imageConfiguracao);
+        imageConfiguracao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this,ConfiguracaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageSair = (ImageButton) findViewById(R.id.imageSair);
+        imageSair.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
